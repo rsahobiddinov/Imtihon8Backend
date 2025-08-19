14c23f36012506d35103a1d56c2011abeaa85786
rsahobiddinov <rustamjon.sahobiddinov@gmail.com>
2025-08-19 18:54:25 +0500

    обнова

--- src/core/database/seeders/seeder.service.ts
+++ src/core/database/seeders/seeder.service.ts
@@ -2,11 +2,13 @@ import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
 import { ConfigService } from '@nestjs/config';
 import bcrypt from 'bcrypt';
+
 @Injectable()
 export class SeederService implements OnModuleInit {
   username: string;
   password: string;
   public logger: Logger = new Logger(SeederService.name);
+
   constructor(
     private db: PrismaService,
     private configService: ConfigService,
@@ -14,31 +16,23 @@ export class SeederService implements OnModuleInit {
     this.username = this.configService.get('SUPER_ADMIN_USERNAME') as string;
     this.password = this.configService.get('SUPER_ADMIN_PASSWORD') as string;
   }
+
   onModuleInit() {
     this.initSeeder();
   }
+
   async initSeeder() {
-    try {
-      await this.checkExistingAdmin();
-      await this.createAdmin();
-      this.logger.log('admin created');
-    } catch (error) {
-      this.logger.warn(error.message);
-    }
-  }
-  async checkExistingAdmin() {
-    const findAdmin = await this.db.prisma.user.findFirst({
-      where: {
+    const hashedPassword = await bcrypt.hash(this.password, 12);
+
+    await this.db.prisma.user.upsert({
+      where: { username: this.username },
+      update: {}, 
+      create: {
         username: this.username,
+        password: hashedPassword,
       },
     });
-    if (!findAdmin) return true;
-    throw new Error('admin existed!!');
-  }
-  async createAdmin() {
-    const hashedPassword = await bcrypt.hash(this.password, 12);
-    await this.db.prisma.user.create({
-      data: { username: this.username, password: hashedPassword },
-    });
+
+    this.logger.log('Admin ensured in database');
   }
 }
